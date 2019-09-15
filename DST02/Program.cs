c4f76ebe1b7b70cce4c9e1037e3359285a8e834d
Muntasir-Pranto <fahimmuntasir43@gmail.com>
2019-09-16 02:32:18 +0600

    tes3

---
+++ DST02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 10;
+            //float f = 1.2f;
+            //double d= 4.33;
+            //decimal de = 12.333m;
+            //a =Convert.ToInt16(d);
+
+
+
+            int a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a);
+
+
+           // Console.WriteLine("Hello World!"+a+a.GetType());
+           // MyClass m = new MyClass();
+            //ConsoleApp3.Class.MyClass n = new ConsoleApp3.Class.MyClass();
+
+
+        }
+    }
+}
